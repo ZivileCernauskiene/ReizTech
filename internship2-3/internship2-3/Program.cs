bf441be94b80adce4394375c2786e8df299c50f9
ZivileCernauskiene <labadaba@gmail.com>
2022-02-22 19:22:28 +0200

    2-3 tasks solved
    There is no user inputs, just added a method as I understood a task.

---
+++ internship2-3/internship2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Branch myTry = new Branch();
+
+myTry.branches.Add(new Branch());
+myTry.branches[0].branches.Add(new Branch());
+myTry.branches.Add(new Branch());
+myTry.branches[1].branches.Add(new Branch());
+myTry.branches[1].branches[0].branches.Add(new Branch());
+
+Console.WriteLine(depthFinder(myTry));
+
+
+
+int depthFinder(Branch structure)
+{
+    if (structure.branches.Count == 0)
+    {
+        return 1;
+    }
+    List<int> result = new List<int>();
+    foreach(var x in structure.branches)
+    {
+        result.Add(depthFinder(x)+1);
+    }
+    return result.Max();
+}
+class Branch
+{
+    public List<Branch> branches=new List<Branch>();
+}
+
